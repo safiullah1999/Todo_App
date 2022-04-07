579b541234991e8ac7cc546edd62e3dfac821692
safiullah1999 <safiullahsk1999@gmail.com>
2022-04-07 15:39:55 +0500

    add, login, register url added for auth case

--- todoapi/urls.py
+++ todoapi/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import TaskList, TaskInfo, CreateTask, DeleteTask, UpdateTask, UpdateTaskStatus, UserDetailView
+from .views import TaskList, TaskInfo, CreateTask, DeleteTask, UpdateTask, UpdateTaskStatus, UserDetailView, TodoListView, Logout
 from .auth import CustomAuthToken
 
 urlpatterns = [
     # TODO LIST with auth
     path('register', UserDetailView.as_view()),
     path('login', CustomAuthToken.as_view()),
+    path('add', TodoListView.as_view()),
+    path('getTodoList/<int:id>', TodoListView.as_view()),
+    path('logout', Logout.as_view()),
 
     # TODO LIST without auth
     path('task-list/', TaskList),
