9bb94798e8825cafa56471a604b38d7c0932b4a0
safiullah1999 <safiullahsk1999@gmail.com>
2022-04-07 15:39:33 +0500

    User Details serializer added

--- todoapi/serializer.py
+++ todoapi/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import SingleTodoList, UserDetail
+from .models import UserDetail, TodoList, SingleTodoList
+
 
 class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,10 +8,23 @@ class UserDetailSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class TodoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TodoList
+        fields = "__all__"
+
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = SingleTodoList
         fields = "__all__"
+
+
+class TodoListDataValidationSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField(max_value=50, min_value=0, required=True)
+    status = serializers.CharField(max_length=15, default="Pending")
+    description = serializers.CharField(max_length=250, default="")
+    title = serializers.CharField(max_length=100, default="task")
 
 
 class TaskDataValidationSerializer(serializers.Serializer):
